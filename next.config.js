b917dd0e4e27e2ee1b15ee1d88f90e82ab3d38c1
Ansh Goyal <anshg1214@gmail.com>
2023-01-01 23:27:18 +0530

    chore(deps): Update Dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	images: {
+		domains: ['picsum.photos', 'avataaars.io'],
+		dangerouslyAllowSVG: true
+	},
 	reactStrictMode: true,
-	swcMinify: true
+	swcMinify: true,
+	typescript: {
+		ignoreBuildErrors: true
+	}
 };
 
 module.exports = nextConfig;
